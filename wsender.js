5e3d927afed315d91bd68037d94a4b8111d6fba1
Kentcoyj <100324513+Kentcoyj@users.noreply.github.com>
2022-03-28 09:45:30 +0700

    Update wsender.js

--- wsender.js
+++ wsender.js
@@ -1,51 +1,91 @@
 const readline = require('readline');
+
 const {
+
     Webhook,
+
     MessageBuilder
+
 } = require('discord-webhook-node');
-var Bg = require('prompt-sync')();
 
-const webhook = Bg("Your Webhook URL= ")
-const webhook_username = Bg("Your Webhook Username= ")
-const webhook_avatar = Bg("Avatar Webhook= ")
+const config = require("./config.json");
 
+const webhook = new Webhook(config.webhook_url);
+
+const webhook_avatar = config.httpavatar;
+
+webhook.setUsername(config.webhook_username);
+
+webhook.setAvatar(config.webhook_avatar);
 
 function main() {
+
     return new Promise(function(resolve, reject) {
+
         let rl = readline.createInterface(process.stdin, process.stdout)
+
         console.log("Super Simple webhook executor written on NodeJS (C)\n")
 
-        rl.setPrompt(`SendMessage@${webhook_username}: `);
+        rl.setPrompt(`SendMessage@${config.webhook_username}: `);
+
         rl.prompt();
+
         rl.on('line', function(command, error) {
+
             if (command === "!exit" || command === "!quit" || command == '!q') {
+
                 console.log('bye')
+
                 process.exit(1)
+
             }
+
             if (command == "!clear") {
+
                 console.clear();
+
             } else {
+
                 if (!command) {
+
                     console.log(`please enter the message`)
+
                 } else {
+
                     try {
+
                         webhook.send(command)
+
                     } catch (err) {
+
                         return console.log("error goblog");
+
                     }
+
                 }
+
             }
+
             rl.prompt()
 
         })
+
     })
+
 }
 
 function run() {
+
     try {
+
         main()
+
     } catch (err) {
+
         return console.log("Something went wrong.");
+
     }
+
 }
+
 run()
