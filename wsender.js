f6c966e9f60113c2055ffaf74ecab5239d83634e
Kentcoyj <100324513+Kentcoyj@users.noreply.github.com>
2022-03-28 09:39:52 +0700

    Add files via upload

--- wsender.js
+++ wsender.js
@@ -3,14 +3,11 @@ const {
     Webhook,
     MessageBuilder
 } = require('discord-webhook-node');
-const config = require("./config.json");
+var Bg = require('prompt-sync')();
 
-
-
-const webhook = new Webhook(config.webhook_url);
-const webhook_avatar = config.httpavatar;
-webhook.setUsername(config.webhook_username);
-webhook.setAvatar(config.webhook_avatar);
+const webhook = Bg("Your Webhook URL= ")
+const webhook_username = Bg("Your Webhook Username= ")
+const webhook_avatar = Bg("Avatar Webhook= ")
 
 
 function main() {
@@ -18,7 +15,7 @@ function main() {
         let rl = readline.createInterface(process.stdin, process.stdout)
         console.log("Super Simple webhook executor written on NodeJS (C)\n")
 
-        rl.setPrompt(`SendMessage@${config.webhook_username}: `);
+        rl.setPrompt(`SendMessage@${webhook_username}: `);
         rl.prompt();
         rl.on('line', function(command, error) {
             if (command === "!exit" || command === "!quit" || command == '!q') {
